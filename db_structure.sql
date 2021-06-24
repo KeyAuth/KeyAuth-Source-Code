cf3f08dba47e89f215414aec6c26c97b0d3e7515
William Nelson <83034852+wnelson03@users.noreply.github.com>
2021-06-24 10:48:46 -0400

    Update Database Structure File (added the configuration settings, no column changes)

--- db_structure.sql
+++ db_structure.sql
@@ -1,3 +1,23 @@
+-- phpMyAdmin SQL Dump
+-- version OVH
+-- https://www.phpmyadmin.net/
+--
+-- Host: omitted
+-- Generation Time: Jun 23, 2021 at 11:06 PM
+-- Server version: 5.6.50-log
+-- PHP Version: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Table structure for table `acclogs`
 --
