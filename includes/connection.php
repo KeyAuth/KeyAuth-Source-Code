0c97b89d835dcd27ecc718696559c9f574842f7a
William Nelson <83034852+wnelson03@users.noreply.github.com>
2021-07-17 00:08:16 -0400

    Added headers for security, disable error reporting
    The reason this is being set in PHP rather than via htaccess is because FluxCDN doesn't have the mod_headers apache module installed. Either way, same effect.

--- includes/connection.php
+++ includes/connection.php
@@ -1,4 +1,19 @@
 <?php
+
+// set headers for increased security. I would set via htaccess though FluxCDN doesn't have mod_security apache module enabled
+header("x-xss-protection: 1; mode=block");
+header("strict-transport-security: max-age=31536000; includeSubDomains; preload");
+header("Permissions-Policy: interest-cohort=()");
+header("x-content-type-options: nosniff");
+header("x-frame-options: DENY");
+header("Referrer-Policy: no-referrer");
+
+// recommended to minimize the attackers knowledge of potential vulreabilities in PHP version
+header_remove("X-Powered-By");
+
+// disable PHP errors
+error_reporting(0);
+
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 $link = mysqli_connect("localhost", "dbusername", "dbpassword", "dbname");
