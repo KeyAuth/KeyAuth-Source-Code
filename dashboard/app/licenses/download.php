33608ce22fc66c3f3e3a7d20ca6c2d3c659bebaa
William Nelson <83034852+wnelson03@users.noreply.github.com>
2022-02-17 15:08:56 -0500

    Update download.php

--- dashboard/app/licenses/download.php
+++ dashboard/app/licenses/download.php
@@ -1,61 +1,106 @@
-<?php
-
-
-
-ini_set('display_errors', 'Off');
-
-error_reporting(0);
-
-
-
-include '../../../includes/connection.php';
-
-session_start();
-
-
-
-$myFile = "KeyAuthKeys.txt";
-
-$fo = fopen($myFile, 'w') or die("can't open file");
-
-
-
-$result = mysqli_query($link, "SELECT * FROM `keys` WHERE `app` = '".$_SESSION['app']."'");
-
-while ($row = mysqli_fetch_array($result))
-
-$stringData.="".$row['key']."\n";
-
-fwrite($fo, $stringData);
-
-fclose($fo);
-
-file_put_contents($myFile,
-    preg_replace(
-        '~[\r\n]+~',
-        "\r\n",
-        trim(file_get_contents($myFile))
-    )
-);
-
-header('Content-Description: File Transfer');
-
-header('Content-Type: application/octet-stream');
-
-header('Content-Disposition: attachment; filename="'.basename($myFile).'"');
-
-header('Expires: 0');
-
-header('Cache-Control: must-revalidate');
-
-header('Pragma: public');
-
-header('Content-Length: ' . filesize($myFile));
-
-readfile($myFile);
-
-unlink($myFile);
-
-exit;
-
+<?php
+
+
+
+
+
+
+
+ini_set('display_errors', 'Off');
+
+
+
+error_reporting(0);
+
+
+
+
+
+
+
+include '../../../includes/connection.php';
+
+
+
+session_start();
+
+
+
+
+
+
+
+// $myFile = "KeyAuthKeys.txt";
+// 
+// 
+// 
+// $fo = fopen($myFile, 'w') or die("can't open file");
+
+
+
+
+
+
+
+$result = mysqli_query($link, "SELECT * FROM `keys` WHERE `app` = '".$_SESSION['app']."'");
+
+
+
+while ($row = mysqli_fetch_array($result))
+
+
+
+$stringData.="".$row['key']."\n";
+
+
+
+// fwrite($fo, $stringData);
+// 
+// 
+// fclose($fo);
+
+
+
+$stringData = preg_replace(
+
+        '~[\r\n]+~',
+
+        "\r\n",
+
+        trim($stringData)
+
+    );
+
+
+
+header('Content-Description: File Transfer');
+
+
+
+header('Content-Type: application/octet-stream');
+
+
+
+header('Content-Disposition: attachment; filename="KeyAuthKeys.txt"');
+
+
+
+header('Expires: 0');
+
+
+
+header('Cache-Control: must-revalidate');
+
+
+
+header('Pragma: public');
+
+
+
+header('Content-Length: ' . strlen($stringData));
+
+
+die($stringData);
+
+
 ?>
