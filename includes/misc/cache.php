eba3181eb808afe4100ba49fe5e7062387c8c758
William Nelson <83034852+wnelson03@users.noreply.github.com>
2022-09-16 19:24:22 -0400

    remove excess code from cache function

--- includes/misc/cache.php
+++ includes/misc/cache.php
@@ -49,53 +49,6 @@ function fetch($redisKey, $sqlQuery, $multiRowed, $expiry = null)
 			$ttl = $data["expiry"] - time();
 			$redis->expire($redisKey, $ttl);
 		}
-		
-		if($redisKey == "KeyAuthStats") {
-			$channels = [1014706457654079520, 1014706498552733756, 1014706542764892230, 1014706585345462293];
-			$values = [$data['numAccs'], $data['numApps'], $data['numKeys'], $data['numOnlineUsers']];
-			
-			$i = 0;
-			while($i < 4) {
-				$url = "https://discord.com/api/v9/channels/$channels[$i]";
-			
-				$curl = curl_init($url);
-				curl_setopt($curl, CURLOPT_URL, $url);
-				curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PATCH');
-				curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-				
-				$headers = array(
-				"user-agent: KeyAuth",
-				"Authorization: Bot MTAxNDY5OTA0NDAzMzAxOTk5NQ.GADQ-6.IkkVy_sJ3ggiNNhASj7eKpjXDKL41UqcGKYlyM",
-				"Content-Type: application/json",
-				);
-				curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-				
-				$prefix = "";
-				switch($i) {
-					case 0:
-						$prefix = "Accounts";
-						break;
-					case 1:
-						$prefix = "Applications";
-						break;
-					case 2:
-						$prefix = "Licenses";
-						break;
-					case 3:
-						$prefix = "Active Users";
-						break;
-				}
-				
-				$body = '{"name":"'.$prefix.': '.$values[$i].'"}';
-				
-				curl_setopt($curl, CURLOPT_POSTFIELDS, $body);
-			
-				$resp = curl_exec($curl);
-				curl_close($curl);
-				
-				$i++;
-			}
-		}
 
 		if (!is_null($expiry)) {
 			$redis->expire($redisKey, $expiry); // set TTL if specified
