bc391bfda31863366cacd9b5329fc97d1e9ad85a
It's Networking <87145183+Its-Networking@users.noreply.github.com>
2023-12-07 16:26:37 -0500

    Update account-logs.php

--- app/pages/account-logs.php
+++ app/pages/account-logs.php
@@ -1,45 +1,53 @@
-<!--begin::Container-->
-<div id="kt_content_container" class="container-xxl">
-    <script src="https://cdn.keyauth.cc/dashboard/unixtolocal.js"></script>
+<?php
+if ($_SESSION['username'] == "demodeveloper" || $_SESSION['username'] == "demoseller") 
+{
+   dashboard\primary\error("Due to privacy purposes, demo accounts do not have access to account logs!");
+   die();
+}
+?>
+<div class="p-4 bg-[#09090d] block sm:flex items-center justify-between lg:mt-1.5">
+    <div class="mb-1 w-full bg-[#0f0f17] rounded-xl">
+        <div class="mb-4 p-4">
+            <?php require '../app/layout/breadcrumb.php'; ?>
+            <h1 class="text-xl font-semibold text-white-900 sm:text-2xl">Account Logs</h1>
+            <p class="text-xs text-gray-500">View the event history of your account.</p>
+            <div class="p-4 flex flex-col">
+                <div class="overflow-x-auto">
+                    <!-- START TABLE -->
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg pt-5">
+                        <table id="kt_datatable_account_logs"
+                            class="w-full text-sm text-left text-gray-500">
+                            <thead>
+                                <tr class="fw-bolder fs-6 text-blue-700 px-7">
+                                    <th class="px-6 py-3">Date</th>
+                                    <th class="px-6 py-3">IP</th>
+                                    <th class="px-6 py-3">User-Agent</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                    $query = misc\mysql\query("SELECT * FROM `acclogs` WHERE `username` = ?", [$_SESSION['username']]);
+                                    $rows = array();
+                                    while ($r = mysqli_fetch_assoc($query->result)) 
+                                    {
+                                        $rows[] = $r;
+                                    }
 
-    <table id="kt_datatable_account_logs" class="table table-striped table-row-bordered gy-5 gs-7 border rounded">
-        <thead>
-            <tr class="fw-bolder fs-6 text-gray-800 px-7">
-                <th>Date</th>
-                <th>IP Address</th>
-                <th>User Agent</th>
-            </tr>
-        </thead>
+                                    foreach ($rows as $row) 
+                                    {
+                                    ?>
+                                        <tr>
+                                            <td>
+                                                <script>
+                                                    document.write(convertTimestamp(<?= $row["date"]; ?>));
+                                                </script>
+                                            </td>
 
-        <tbody>
-            <?php
-            $query = misc\mysql\query("SELECT * FROM `acclogs` WHERE `username` = ?", [$_SESSION['username']]);
-            $rows = array();
-            while ($r = mysqli_fetch_assoc($query->result)) 
-            {
-                $rows[] = $r;
-            }
-
-            foreach ($rows as $row) 
-            {
-            ?>
-
-                <tr>
-                    <td>
-                        <script>
-                            document.write(convertTimestamp(<?php echo $row["date"]; ?>));
-                        </script>
-                    </td>
-
-                    <td><?php echo $row["ip"]; ?></td>
-
-                    <td><?php echo $row["useragent"]; ?></td>
-                </tr>
-            <?php
-            }
-
-            ?>
-        </tbody>
-    </table>
-</div>
-<!--end::Container-->
+                                            <td><a class="blur-sm hover:blur-none"><?= $row["ip"]; ?></a></td>
+                                            <td><a class="blur-sm hover:blur-none"><?= $row["useragent"]; ?></a></td>
+                                        </tr>
+                                    <?php
+                                    }
+                                ?>
+                            </tbody>
+                        </table>
