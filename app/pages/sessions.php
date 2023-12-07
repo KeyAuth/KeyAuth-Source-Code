c5e354efc04db644af367468a9316cfdb3a509da
It's Networking <87145183+Its-Networking@users.noreply.github.com>
2023-12-07 16:43:06 -0500

    Update sessions.php

--- app/pages/sessions.php
+++ app/pages/sessions.php
@@ -4,110 +4,99 @@ if ($_SESSION['role'] == "Reseller") {
     die();
 }
 if($role == "Manager" && !($permissions & 16)) {
-    die('You weren\'t granted permissions to view this page.');
+    misc\auditLog\send("Attempted (and failed) to view sessions.");
+    dashboard\primary\error("You weren't granted permission to view this page!");
+    die();
 }
 if(!isset($_SESSION['app'])) {
+    dashboard\primary\error("Application not selected");
     die("Application not selected.");
 }
 if (isset($_POST['killall'])) {
     $resp = misc\session\killAll();
-    switch ($resp) {
-        case 'failure':
-            dashboard\primary\error("Failed to kill all sessions!");
-            break;
-        case 'success':
-            dashboard\primary\success("Successfully killed all sessions!");
-            break;
-        default:
-            dashboard\primary\error("Unhandled Error! Contact us if you need help");
-            break;
-    }
+    match($resp){
+        'failure' => dashboard\primary\error("Failed to kill all sessions!"),
+        'success' => dashboard\primary\success("Successfully killed all sessions!"),
+        default => dashboard\primary\error("Unhandled Error! Contact us if you need help")
+    };
 }
 if (isset($_POST['kill'])) {
     $resp = misc\session\killSingular($_POST['kill']);
-    switch ($resp) {
-        case 'failure':
-            dashboard\primary\error("Failed to kill session!");
-            break;
-        case 'success':
-            dashboard\primary\success("Successfully killed session!");
-            break;
-        default:
-            dashboard\primary\error("Unhandled Error! Contact us if you need help");
-            break;
-    }
+    match($resp){
+        'failure' => dashboard\primary\error("Failed to kill session!"),
+        'success' => dashboard\primary\success("Successfully killed session!"),
+        default => dashboard\primary\error("Unhandled Error! Contact us if you need help")
+    };
 }
 ?>
-    <!-- Include the jQuery library -->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-    <script>
-    $(document).ready(function() {
-    $('div.modal-content').css('border', '2px solid #1b8adb');
-    });
-    </script>
-<!--begin::Container-->
-<div id="kt_content_container" class="container-xxl">
+<div class="p-4 bg-[#09090d] block sm:flex items-center justify-between lg:mt-1.5">
+    <div class="mb-1 w-full bg-[#0f0f17] rounded-xl">
+        <div class="mb-4 p-4">
+            <?php require '../app/layout/breadcrumb.php'; ?>
+            <h1 class="text-xl font-semibold text-white-900 sm:text-2xl ">Sessions</h1>
+            <p class="text-xs text-gray-500">All of your active sessions. (Active Users) <a
+                    href="https://keyauth.readme.io/reference/sessions-1" target="_blank"
+                    class="text-blue-600  hover:underline">Learn More</a>.</p>
+            <br>
+            <div class="p-4 flex flex-col">
+                <div class="overflow-x-auto">
+                    <!-- Session Functions -->
+                    <button
+                        class="inline-flex text-white bg-red-700 hover:opacity-60 focus:ring-0 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 transition duration-200"
+                        data-modal-toggle="kill-all-sessions-modal" data-modal-target="kill-all-sessions-modal">
+                        <i class="lni lni-trash-can mr-2 mt-1"></i>Kill All Sessions
+                    </button>
+                    <!-- End Sessions Functions -->
 
-    <form method="post">
-        <button type="button" data-bs-toggle="modal" data-bs-target="#killallsessions"
-            class="dt-button buttons-print btn btn-danger mr-1"><i class="fas fa-trash-alt fa-sm text-white-50"></i>
-            Kill All Sessions</button>
-    </form>
-
-    <br>
-    <script src="https://cdn.keyauth.cc/dashboard/unixtolocal.js"></script>
-    <table id="kt_datatable_sessions" class="table table-striped table-row-bordered gy-5 gs-7 border rounded">
-        <thead>
-            <tr class="fw-bolder fs-6 text-gray-800 px-7">
-                <th>ID</th>
-                <th>Credential</th>
-                <th>Expires</th>
-                <th>Authenticated</th>
-                <th>IP Address</th>
-                <th>Manage</th>
-            </tr>
-        </thead>
-    </table>
-
-
-    <div class="modal fade" tabindex="-1" id="killallsessions">
-        <!--begin::Modal dialog-->
-        <div class="modal-dialog modal-dialog-centered mw-900px">
-            <!--begin::Modal content-->
-            <div class="modal-content">
-                <!--begin::Modal header-->
-                <div class="modal-header">
-                    <h2 class="modal-title">Kill All Sessions</h2>
-
-                    <!--begin::Close-->
-                    <div class="btn btn-sm btn-icon btn-active-color-primary" data-bs-dismiss="modal">
-                        <span class="svg-icon svg-icon-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none">
-                                <rect opacity="0.5" x="6" y="17.3137" width="16" height="2" rx="1"
-                                    transform="rotate(-45 6 17.3137)" fill="black" />
-                                <rect x="7.41422" y="6" width="16" height="2" rx="1" transform="rotate(45 7.41422 6)"
-                                    fill="black" />
-                            </svg>
-                        </span>
+                    <!--Kill All Sessions Modal -->
+                    <div id="kill-all-sessions-modal" tabindex="-1"
+                        class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                        <div class="relative w-full max-w-md max-h-full">
+                            <div class="relative bg-[#0f0f17] border border-red-700 rounded-lg shadow">
+                                <div class="p-6 text-center">
+                                    <div class="flex items-center p-4 mb-4 text-sm text-white border border-yellow-500 rounded-lg bg-[#0f0f17]"
+                                        role="alert">
+                                        <span class="sr-only">Info</span>
+                                        <div>
+                                            <span class="font-medium">Notice!</span> You're about to kill all of
+                                            your active sessions. Requiring everyone to log into your app again.
+                                        </div>
+                                    </div>
+                                    <h3 class="mb-5 text-lg font-normal text-gray-200">Are you sure
+                                        you want
+                                        to
+                                        kill your active sessions?</h3>
+                                    <form method="POST">
+                                        <button data-modal-hide="kill-all-sessions-modal" name="killall"
+                                            class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                                            Yes, I'm sure
+                                        </button>
+                                        <button data-modal-hide="kill-all-sessions-modal" type="button"
+                                            class="inline-flex text-white bg-gray-700 hover:opacity-60 focus:ring-0 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 transition duration-200">No,
+                                            cancel</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <!--end::Close-->
-                </div>
-                <div class="modal-body">
-                    <label class="fs-5 fw-bold mb-2">
-                        <p> Are you sure you want to delete all sessions? This can not be undone.</p>
-                    </label>
-                </div>
-                <div class="modal-footer">
-                    <form method="post">
-                        <button class="btn btn-light" data-bs-dismiss="modal">No</button>
-                        <button name="killall" class="btn btn-danger">Yes</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
+                    <!-- End Kill All Sessions Modal -->
 
-</div>
-<!--end::Container-->
+                    <!-- START TABLE -->
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg pt-5">
+                        <table id="kt_datatable_sessions" class="w-full text-sm text-left text-white">
+                            <thead>
+                                <tr class="fw-bolder fs-6 text-blue-700 px-7">
+                                    <th class="px-6 py-3">ID</th>
+                                    <th class="px-6 py-3">Credentials</th>
+                                    <th class="px-6 py-3">Expires</th>
+                                    <th class="px-6 py-3">Authenticated</th>
+                                    <th class="px-6 py-3">IP Adress</th>
+                                    <th class="px-6 py-3">Action</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                    <p class="text-xs text-red-600">Dropdown actions in <b>RED</b> do not show a confirmation!<a
+                            class="text-blue-700"> Dropdown actions in <b>BLUE</b> will show a confirmation!</a></p>
+                    
