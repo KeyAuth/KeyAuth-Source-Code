3fed550af2775ae9eb931e56a96af91c4cba6b04
It's Networking <87145183+Its-Networking@users.noreply.github.com>
2023-12-07 16:49:23 -0500

    Update and rename _footer.php to footer.php

---
+++ app/layout/footer.php
@@ -0,0 +1,17 @@
+<footer class="bg-[#0f0f17] rounded-lg shadow m-4">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm text-white-500 sm:text-center">Copyright © 2020 - <script>document.write(new Date().getFullYear())</script>  KeyAuth LLC. All Rights Reserved.
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium sm:mt-0">
+        <li>
+            <a href="https://github.com/KeyAuth/KeyAuth-Source-Code" target="_blank" class="mr-4 hover:underline md:mr-6 ">Source Code</a>
+        </li>
+        <li>
+            <a href="https://betav3.keyauth.cc/terms" target="_blank" class="mr-4 hover:underline md:mr-6">Terms and Conditions</a>
+        </li>
+        <li>
+            <a href="https://keyauth.tawk.help" target="_blank" class="hover:underline">Help</a>
+        </li>
+    </ul>
+    </div>
+</footer>
